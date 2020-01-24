9af50ec93f99cc1fbe175d8863c7c4aeba05d2e7
Michael Main <deusprogrammer@gmail.com>
2020-01-24 12:40:27 -0600

    Added a login page

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
                 <Link to={`${process.env.PUBLIC_URL}/create/users`}>Create User</Link>|
                 <Link to={`${process.env.PUBLIC_URL}/budgets`}>View Budgets</Link>|
                 <Link to={`${process.env.PUBLIC_URL}/create/budgets`}>Create Budget</Link>
+                <Link to={`${process.env.PUBLIC_URL}/login`}>Login</Link>
             </nav>
             <Switch>
                 <Route path={`${process.env.PUBLIC_URL}/create/budgets`} exact component={NewBudget} />
